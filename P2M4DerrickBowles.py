aa17d6c4d5f2c3544ec4ff4525daab960f66fdf6
Derrick Bowles <derrick@smcreset.com>
2021-10-29 23:24:05 -0400

    Finished required code P2M4

---
+++ P2M4DerrickBowles.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("curl https://raw.githubusercontent.com/MicrosoftLearning/intropython/master/world_temp_mean.csv>>mean_temp.txt")
+
+temps = open('mean_temp.txt', 'a+')
+temps.write('Rio de Janeiro,Brazil,30.0,18.0\n')
+temps.seek(0)
+headings = temps.readline()
+theadings = headings.split(',')
+city_temp = temps.readline().split(',')
+
+while city_temp:
+	print(headings[2], 'for', city_temp[0], 'is', city_temp[2])
+	if city_temp[0] == 'Tokyo':
+		break
+	else:
+		city_temp = temps.readline().split(',')
+	
